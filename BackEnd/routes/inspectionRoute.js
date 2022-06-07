3e766fb0d39cfb0d7e3f869544ffe2efade1dd70
leducUET <57931022+leducUET@users.noreply.github.com>
2022-06-07 17:44:40 +0700

    api premises

--- BackEnd/routes/inspectionRoute.js
+++ BackEnd/routes/inspectionRoute.js
@@ -4,9 +4,9 @@ const { createInspection } = require("../controllers/inspectionController");
 const router = express.Router();
 const { verifyToken } = require("../middleware/authMiddleware");
 
-router.get("/get-inspections", verifyToken, getInspections);
-router.post("/create-inspection", verifyToken, createInspection);
-router.put("/edit-inspection", verifyToken, editINspection);
-router.delete("/delete-inspection/:_id", verifyToken, deleteINspection);
+// router.get("/get-inspections", verifyToken, getInspections);
+// router.post("/create-inspection", verifyToken, createInspection);
+// router.put("/edit-inspection", verifyToken, editINspection);
+// router.delete("/delete-inspection/:_id", verifyToken, deleteINspection);
 
 module.exports = router;
